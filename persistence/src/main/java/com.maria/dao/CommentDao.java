81f79cf1218aded6815f1a0f9087bcde170948ea
MariaSimion <marialexandra.simion@gmail.com>
2015-08-28 15:24:31 +0300

    service comment added

---
+++ persistence/src/main/java/com.maria/dao/CommentDao.java
@@ -0,0 +1,7 @@
+package com.maria.dao;
+
+/**
+ * Created by msimion on 8/28/2015.
+ */
+public interface CommentDao {
+}
---
+++ service/src/main/com.maria.java/ServiceComment.java
@@ -0,0 +1,5 @@
+/**
+ * Created by msimion on 8/28/2015.
+ */
+public class ServiceComment {
+}
